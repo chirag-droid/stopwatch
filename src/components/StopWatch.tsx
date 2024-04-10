d7439ef814f5296f6d3704fb6f5b4e5abf23d0f8
chirag-droid <chirag.singla.pi@gmail.com>
2024-04-10 09:05:45 +0530

    use RequestAnimeFrame for performance

--- src/components/StopWatch.tsx
+++ src/components/StopWatch.tsx
@@ -2,32 +2,48 @@ import { useEffect, useState } from "react"
 import styles from './StopWatch.module.css'
 
 const StopWatch = () => {
+    const [startTime, setStartTime] = useState(-1);
     const [elapsedTime, setElapsedTime] = useState(0);
     const [isPaused, setIsPaused] = useState(true);
 
-    const secondsElapsed = Math.floor(elapsedTime / 1000)
-    const minutesElapsed = Math.floor(secondsElapsed / 60)
-    const hoursElapsed = Math.floor(minutesElapsed / 60)
+    useEffect(() => {
+        let animationId: number | undefined;
 
-    const milliSeconds = String(elapsedTime % 1000).padStart(3, '0')
-    const seconds = String(secondsElapsed % 60).padStart(2, '0')
-    const minutes = String(minutesElapsed % 60).padStart(2, '0')
-    const hours = String(hoursElapsed % 60).padStart(2, '0')
+        const updateTimer = () => {
+            const now = Date.now();
+            setElapsedTime(now - startTime);
+        };
+
+        if (!isPaused) {
+            if (startTime <= 0) {
+                setStartTime(Date.now() - elapsedTime);
+            }
+            animationId = requestAnimationFrame(updateTimer)
+        } else {
+            if (animationId) {
+                cancelAnimationFrame(animationId)
+            }
+        }
+
+        return () => { 
+            if (animationId) cancelAnimationFrame(animationId) 
+        };
+    }, [isPaused, startTime, elapsedTime]);
+
+    const secondsElapsed = Math.floor(elapsedTime / 1000);
+    const minutesElapsed = Math.floor(secondsElapsed / 60);
+    const hoursElapsed = Math.floor(minutesElapsed / 60);
+
+    const milliSeconds = String(elapsedTime % 1000).padStart(3, '0');
+    const seconds = String(secondsElapsed % 60).padStart(2, '0');
+    const minutes = String(minutesElapsed % 60).padStart(2, '0');
+    const hours = String(hoursElapsed).padStart(2, '0');
 
     const reset = () => {
-        setIsPaused(true)
+        setIsPaused(true);
+        setStartTime(-1);
         setElapsedTime(0);
     }
-
-    useEffect(() => {
-        if (!isPaused) {
-            const interval = setInterval(() => {
-                setElapsedTime(elapsedTime => elapsedTime + 10);
-            }, 10)
-
-            return () => clearInterval(interval)
-        }
-    }, [isPaused, setElapsedTime])
 
     return (
         <div className={styles.topContainer}>
@@ -49,7 +65,7 @@ const StopWatch = () => {
             </div>
 
             <div className={styles.buttonContainer}>
-                <button onClick={() => setIsPaused((state) => !state)}>
+                <button onClick={() => setIsPaused(!isPaused)}>
                     {isPaused ? `${elapsedTime === 0 ? "Start" : "Resume"}` : "Pause"}
                 </button>
                 <button onClick={reset}>Reset</button>
@@ -58,4 +74,4 @@ const StopWatch = () => {
     )
 }
 
-export default StopWatch
+export default StopWatch;
